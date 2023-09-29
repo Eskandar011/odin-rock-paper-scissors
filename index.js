4bda0433d259bc6ef502f11f9a3cdf8b24339512
Eskandar011 <alejandro.lima11vaz@gmail.com>
2023-09-29 12:44:52 -0600

    Creacion de la funcion game y actualizacion de la funcion playRound

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //Score
 
+let scorePlayer = 0;
+let scoreComputer = 0;
+
 function getComputerChoice() {
   const random = Math.round(Math.random() * (3 - 1) + 1);
   switch (random) {
@@ -18,10 +21,41 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `You win!`;
+    scorePlayer += 1;
+    return `You win!\nScore:\nPlayer:${scorePlayer}\nComputer:${scoreComputer}`;
   } else if (playerSelection === computerSelection) {
-    return `It's a tie!`;
+    return `It's a tie!\nScore:\nPlayer:${scorePlayer}\nComputer:${scoreComputer}`;
   } else {
-    return `You lose!`;
+    scoreComputer += 1;
+    return `You lose!\nScore:\nPlayer:${scorePlayer}\nComputer:${scoreComputer}`;
   }
 }
+
+function game() {
+  const computerSelection = getComputerChoice();
+  let playerSelection;
+
+  playerSelection = prompt("Enter 'rock', 'scissors' or 'paper'.");
+  console.log(playRound(playerSelection, computerSelection));
+  playerSelection = prompt("Enter 'rock', 'scissors' or 'paper'.");
+  console.log(playRound(playerSelection, computerSelection));
+  playerSelection = prompt("Enter 'rock', 'scissors' or 'paper'.");
+  console.log(playRound(playerSelection, computerSelection));
+  playerSelection = prompt("Enter 'rock', 'scissors' or 'paper'.");
+  console.log(playRound(playerSelection, computerSelection));
+  playerSelection = prompt("Enter 'rock', 'scissors' or 'paper'.");
+  console.log(playRound(playerSelection, computerSelection));
+
+  if (scorePlayer === 5) {
+    alert(
+      `You win!!!! (Final Score:\nPlayer:${scorePlayer}\nComputer:${scoreComputer})`
+    );
+  }
+  if (scoreComputer === 5) {
+    alert(
+      `You lose!!!! (Final Score:\nPlayer:${scorePlayer}\nComputer:${scoreComputer})`
+    );
+  }
+}
+
+game();
